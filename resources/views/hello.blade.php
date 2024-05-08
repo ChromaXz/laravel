1e4b33a571adf056b05d3910b749d57fc0ac9d8a
ChromaXz <dewaandika02@gmail.com>
2024-05-08 13:07:09 +0800

    Commit latihan

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -7,5 +7,18 @@
 </head>
 <body>
     <h1>Hello ini file pertama saya di view laravel</h1>
+    @php
+    $nama = 'Budi';
+    $nilai = 60;
+    @endphp
+    {{-- ini komentar di blade engine --}}
+    @if ($nilai >= 60)
+    @php $ket = "lulus"; @endphp
+    @else
+    @php $ket = "Tidak Lulus"; @endphp
+    @endif
+
+    {{-- memanggil variabel hasil didalam laravel menggunakan kurung kurawal --}}
+    {{$nama}} <p> Dengan nilai </p> {{$nilai}} <p> Dinyatakan </p> {{$ket}}
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,37 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
+Route::get('/', function(){
+    return view('front.home');
+});
+//contoh routing untuk mengarahkan ke view, tanpa melalui controller
 Route::get('/percobaan_pertama', function(){
     return view('hello');
 });
+//contoh routing yang mengarahkan ke dirinya sendiri, tanpa view ataupun conroller
+Route::get('/salam', function(){
+    return "<h1>Selamat Pagi Peserta MSIB</h1>";
+});
+//contoh routing yang menggunakan parameter
+Route::get('/staff/{nama}/{divisi}', function($nama, $divisi){
+    return 'Nama Pegawai '.$nama.'<br> Departemen: '.$divisi;
+});
+
+Route::get('/daftar_nilai', function(){
+    //return view yang mengarahkan kedalam view yang didalamnya ada folder nilai dan file daftar_nilai
+    return view('nilai.daftar_nilai');
+});
+
+Route::get('/dashboard', function(){
+    return view('admin.dashboard');
+});
+
+
+
 
 //tugas kelompok
 //1. buat repository github untuk tugas akhir
